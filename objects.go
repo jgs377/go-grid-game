9b2779f1c7c534f0a68aca8f29b60d8137082bb4
Justus Schmidt <jgschm2@gmail.com>
2023-10-19 20:41:43 -0400

    object structs and functions

--- objects.go
+++ objects.go
@@ -7,46 +7,84 @@ type object struct {
 	windowY  float64
 	location Coord
 
-	objectType string
+	// objectType string
 
 	// TODO: look into making separate structs for different objects
-	direction float64 // TODO: improve; -1 = left, 1 = right
 }
 
 type Player struct {
-	*object
+	object
 
-	score int
+	score     int
+	direction float64 // TODO: improve; -1 = left, 1 = right
 }
 
 type Obstacle struct {
-	*object
+	object
 
 	// In case we allow breaking obstacles in the future
-	breakable    bool
-	broken       bool
-	breakPenalty float64
-	walkPenalty  float64
+	// breakable    bool
+	// broken       bool
+	// breakPenalty float64
+	// walkPenalty  float64
 }
 
 type Reward struct {
-	*object
-
+	object
 	value int
 }
 
-type EndCondition struct {
-	*object
-
+type LossCondition struct {
+	object
 	value int
 }
 
+type WinCondition struct {
+	object
+	value int
+}
+
+// All object types present in the game implement this ObjectInterface
 type ObjectInterface interface {
 	Draw()
 }
 
-func (obj *object) Draw() {
+func (p Player) Draw() {
 	// TODO
 }
 
-func (obj *object) loadAssets() {}
+func (o Obstacle) Draw() {
+	// TODO
+}
+
+func (r Reward) Draw() {
+	// TODO
+}
+
+func (w WinCondition) Draw() {
+	// TODO
+}
+
+func (l LossCondition) Draw() {
+	// TODO
+}
+
+func NewPlayer() (player Player) {
+	player = Player{object{25, 25, Coord{0, 0}}, 0, 1}
+	return player
+}
+
+func NewObstacle() (obstacle Obstacle) {
+	obstacle = Obstacle{object{25, 25, Coord{0, 0}}}
+	return obstacle
+}
+
+func NewWinCondition() (winCondition WinCondition) {
+	winCondition = WinCondition{object{25, 25, Coord{0, 0}}, 100}
+	return winCondition
+}
+
+func NewLossCondition() (lossCondition LossCondition) {
+	lossCondition = LossCondition{object{25, 25, Coord{0, 0}}, -100}
+	return lossCondition
+}
